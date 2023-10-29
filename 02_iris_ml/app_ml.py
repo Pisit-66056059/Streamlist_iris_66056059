cf2d0018c56c5c431d87192630516e63f041be71
Pisit Chingulprasan <66056059@kmitl.ac.th>
2023-10-29 19:46:33 +0700

    Run and add feature importance

--- 02_iris_ml/app_ml.py
+++ 02_iris_ml/app_ml.py
@@ -1,47 +1,43 @@
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import accuracy_score 
+from sklearn.metrics import accuracy_score
 
-from sklearn.ensemble import RandomForestClassifier 
+from sklearn.ensemble import RandomForestClassifier
 
-from sklearn.model_selection import train_test_split 
+from sklearn.model_selection import train_test_split
 
-import pickle 
-
- 
+import pickle
 
 iris_df = pd.read_csv('iris.csv')
 iris_df.dropna(inplace=True)
 output = iris_df['variety']
 features = iris_df[['sepal.length', 'sepal.width', 'petal.length', 'petal.width']]
-features = pd.get_dummies(features) 
-output, uniques = pd.factorize(output) 
+features = pd.get_dummies(features)
+output, uniques = pd.factorize(output)
 
 x_train, x_test, y_train, y_test = train_test_split(
-	features, output, test_size=.8) 
-rfc = RandomForestClassifier(random_state=15) 
-rfc.fit(x_train, y_train) 
-y_pred = rfc.predict(x_test) 
-score = accuracy_score(y_pred, y_test) 
-print('Our accuracy score for this model is {}'.format(score)) 
+    features, output, test_size=.8)
+rfc = RandomForestClassifier(random_state=15)
+rfc.fit(x_train, y_train)
+y_pred = rfc.predict(x_test)
+score = accuracy_score(y_pred, y_test)
+print('Our accuracy score for this model is {}'.format(score))
 
- 
+rf_pickle = open('random_forest_penguin.pickle', 'wb')
+pickle.dump(rfc, rf_pickle)
+rf_pickle.close()
+output_pickle = open('output_penguin.pickle', 'wb')
+pickle.dump(uniques, output_pickle)
+output_pickle.close()
 
-rf_pickle = open('random_forest_penguin.pickle', 'wb') 
-pickle.dump(rfc, rf_pickle) 
-rf_pickle.close() 
-output_pickle = open('output_penguin.pickle', 'wb') 
-pickle.dump(uniques, output_pickle) 
-output_pickle.close() 
+fig, ax = plt.subplots()
 
- 
+ax = sns.barplot(x=rfc.feature_importances_, y=features.columns)
+plt.title('Which features are the most important for species prediction?')
+plt.xlabel('Importance')
+plt.ylabel('Feature')
+plt.tight_layout()
+fig.savefig('feature_importance.png')
 
-fig, ax = plt.subplots() 
-
-ax = sns.barplot(x=rfc.feature_importances_, y=features.columns) 
-plt.title('Which features are the most important for species prediction?') 
-plt.xlabel('Importance') 
-plt.ylabel('Feature') 
-plt.tight_layout() 
-fig.savefig('feature_importance.png') 
+#%%
